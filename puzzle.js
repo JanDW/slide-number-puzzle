102f9a7f65c16a0c7a83339a6695948a8641d4eb
Jan De Wilde <jan@jandewilde.com>
2020-07-04 00:50:21 -0400

    First playable version :rocket:

--- puzzle.js
+++ puzzle.js
@@ -1,40 +1,81 @@
 // @ts-check
-
 "use strict";
 
-let puzzle = [
-  [14, 12, 13, 9],
-  [11, 2, 4, 7],
-  [0, 6, 5, 8],
-  [3, 10, 15, 1],
-];
+// Init animateCSSGrid dependency
+const grid = document.querySelector(".grid");
+const { forceGridAnimation } = animateCSSGrid.wrapGrid(grid);
 
-const rows = puzzle.length;
-const columns = puzzle[0].length;
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const emptyTile = document.querySelector(".tile--empty");
 
-let emptyTileIndexes = findTile(0);
-let currentTileNumber = 1;
-let currentTileLocation = findTile(currentTileNumber);
+const heading = document.querySelector(".heading");
 
-/**
- * @param {number} puzzleNumber
- */
+const areaKeys = {
+  A: ["B", "E"],
+  B: ["A", "C", "F"],
+  C: ["B", "D", "G"],
+  D: ["C", "H"],
+  E: ["F", "A", "I"],
+  F: ["E","G","B","J"],
+  G: ["F","H","C","K"],
+  H: ["G","D","L"],
+  I: ["J", "E", "M"],
+  J: ["I","K","F","N"],
+  K: ["J","L","G","O"],
+  L: ["K","H","P"],
+  M: ["N","I"],
+  N: ["M","O","J"],
+  O: ["N","P","K"],
+  P: ["O", "L"]
+}
 
-function findTile(puzzleNumber) {
-  for (let row = 0; row <= rows; row++) {
-    for (let column = 0; column <= columns; column++) {
-      if (puzzle[row][column] == puzzleNumber) {
-        return [row, column];
-      }
+// Add click listener to all tiles
+tiles.map(tile => {
+	tile.addEventListener("click", event => {
+		// Grab the grid area set on the clicked tile and empty tile
+		const tileArea = tile.style.getPropertyValue("--area");
+		const emptyTileArea = emptyTile.style.getPropertyValue("--area");
+
+		// Swap the empty tile with the clicked tile
+		emptyTile.style.setProperty("--area", tileArea);
+		tile.style.setProperty("--area", emptyTileArea);
+
+		// Animate the tiles
+		forceGridAnimation();
+
+		// Unlock and lock tiles
+		unlockTiles(tileArea);
+	});
+});
+
+const unlockTiles = (currentTileArea) => {
+  // Cycle through all the tiles and check which should be disabled and enabled
+  tiles.map((tile) => {
+    const tileArea = tile.style.getPropertyValue("--area");
+
+    // Check if that areaKey has the tiles area in it's values
+    // .trim() is needed because the animation lib formats the styles attribute
+    if (areaKeys[currentTileArea.trim()].includes(tileArea.trim())) {
+      tile.disabled = false;
+    } else {
+      tile.disabled = true;
     }
+  });
+
+  // Check if the tiles are in the right order
+  isComplete(tiles);
+};
+
+const isComplete = (tiles) => {
+  const currentTilesString = tiles
+    .map((tile) => tile.style.getPropertyValue("--area").trim())
+    .toString();
+  if (currentTilesString == "N,L,M,I,K,B,D,G,P,F,E,H,C,J,O,A") {
+    heading.innerHTML = "Well done, young Padawan!";
+    heading.style = `display:block; animation: popIn .3s cubic-bezier(0.68, -0.55, 0.265, 1.55);`;
   }
-}
+};
 
-function findAdjacentTiles(row,column) {
 
-}
 
-/* array of row,column arrays */
-function findEmptyTile(adjacentTiles) {
 
-}
